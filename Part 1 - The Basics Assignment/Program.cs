b2c9cfbdd90d48d9e7775532c7a0fd5f30fa38c9
362568016 <362568016@2260I-L29-025>
2026-09-11 13:16:15 -0400

    Add project files.

---
+++ Part 1 - The Basics Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿string firstName = "Kaiden";
+string favMovie = "The Matrix".ToUpper();
+
+Console.WriteLine($"Grettings {firstName} Do you wanna watch {favMovie}?".ToLower());
+
+Console.WriteLine(favMovie.Contains("THE"));
+Console.WriteLine(favMovie.Replace("A", "@"));
+Console.WriteLine(favMovie.Replace("E", "3"));
+
+
+string movieQuote = "I Dont Know".ToLower();
+Console.WriteLine(movieQuote.Replace("o", " "));
+
+
+Console.WriteLine("             ,\r\n           __)\\_  \r\n     (\\_.-'    a`-.\r\njgs  (/~~````(/~^^`\r\n");
